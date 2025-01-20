574b82777d22afedf541e9fa3f14ba73757dadbd
David Burt <burt.david.j@gmail.com>
2025-01-20 17:29:47 +1100

    images for deployment

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Title from '../components/Title';
-import { assets } from '../assets';
+import { assets } from '../assets/assets';
 import NewsLetterBox from '../components/NewsLetterBox';
 
 const Contact = () => {
